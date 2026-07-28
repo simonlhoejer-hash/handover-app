a6de81095b8fda14b099e6bbe53a8ecaaba2b9f0
simonlhoejer-hash <simon.l.hoejer@gmail.com>
2026-07-28 14:17:56 +0200

    Add food waste overview route

---
+++ app/galley/food-waste/overblik/page.tsx
@@ -0,0 +1,7 @@
+import FoodWasteStatsPage from '@/components/food-waste/FoodWasteStatsPage'
+
+export const dynamic = 'force-dynamic'
+
+export default function Page() {
+  return <FoodWasteStatsPage />
+}
