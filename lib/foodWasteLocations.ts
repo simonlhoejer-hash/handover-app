3c0de9428c618268fd5364d2e14fe03be9d0ebe7
simonlhoejer-hash <simon.l.hoejer@gmail.com>
2026-07-28 13:37:16 +0200

    Add food waste location slugs

---
+++ lib/foodWasteLocations.ts
@@ -0,0 +1,17 @@
+export const FOOD_WASTE_LOCATIONS = [
+  { name: 'Skagerak morgen', slug: 'skagerak-morgen' },
+  { name: 'Skagerak aften', slug: 'skagerak-aften' },
+  { name: 'Messen morgen', slug: 'messen-morgen' },
+  { name: 'Messen frokost & aften', slug: 'messen-frokost-aften' },
+  { name: 'Commodore morgen', slug: 'commodore-morgen' },
+  { name: 'Produktion Main Galley', slug: 'produktion-main-galley' },
+  { name: 'Produktion Skagerak Galley', slug: 'produktion-skagerak-galley' },
+  { name: 'Produktion Kold Galley', slug: 'produktion-kold-galley' },
+  { name: 'Produktion Bageri', slug: 'produktion-bageri' },
+  { name: 'Produktion Slagteri', slug: 'produktion-slagteri' },
+  { name: 'Produktion Proviant dæk 1', slug: 'produktion-proviant-daek-1' },
+]
+
+export function getFoodWasteLocation(slug: string) {
+  return FOOD_WASTE_LOCATIONS.find((location) => location.slug === slug)
+}
