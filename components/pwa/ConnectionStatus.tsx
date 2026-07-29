77a2acb86eb6968f487e618defb708e0618061ed
Simon <simon.l.hoejer@gmail.com>
2026-07-30 00:40:29 +0200

    Vis online-status foran header

--- components/pwa/ConnectionStatus.tsx
+++ components/pwa/ConnectionStatus.tsx
@@ -45,7 +45,7 @@ export default function ConnectionStatus() {
   const Icon = isOnline ? Cloud : CloudOff
 
   return (
-    <div className="fixed right-3 top-3 z-30 sm:right-4 sm:top-4">
+    <div className="fixed right-3 top-3 z-50 sm:right-4 sm:top-4">
       <button
         type="button"
         onClick={() => setIsExpanded((current) => !current)}
